bf9f1c27d13c84e116692b99c59d98da42e6df8c
Свобода Тимур <svobodatimur@gmail.com>
2021-01-26 15:19:34 +0200

    Create base site structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,30 @@
 module.exports = {
     title: 'Infoxam',
-    description: 'Infoxam is a static site made to spread the information that applies to AM-201 and AM-202'
+    description: 'Infoxam is a static site made to spread the information that applies to AM-201 and AM-202',
+    themeConfig: {
+        sidebar: [
+            {
+                title: 'Общее',
+                collapsable: false,
+                children: [
+                    '/',
+                    '/subjects-schedule',
+                    '/call-schedule'
+                ]
+            },
+            {
+                title: 'Предметы',
+                collapsable: false,
+                children: [
+                    '/english.md',
+                    '/further-math.md',
+                    '/discrete-computing.md',
+                    '/computer-logic.md',
+                    '/programming.md',
+                    '/web-programming.md',
+                    '/physics.md'
+                ]
+            }
+        ]
+    }
 }
