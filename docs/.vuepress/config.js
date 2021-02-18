2c9aa5b1b219909d97cba8210d5ffbb8be3f1920
Timur Svoboda <svobodatimur@gmail.com>
2021-02-18 15:20:10 +0200

    Add PWA plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,27 @@ module.exports =  {
     title: 'Infoxam',
     description: 'Infoxam is a static site made to spread the information that applies to AM-201 and AM-202',
     head: [
-        ['meta', { name: 'theme-color', content: '#ffffff' }],
+        // Icons
         ['link', { rel: 'icon', href: 'favicon.svg' }],
         ['link', { rel: 'mask-icon', href: 'mask-icon.svg', color: '#000000' }],
         ['link', { rel: 'apple-touch-icon', href: 'apple-touch-icon.png' }],
+
+        //Manifest
         ['link', { rel: 'manifest', href: 'manifest.json' }],
+
+        // PWA settings
+        ['meta', { name: 'theme-color', content: '#ffffff' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ],
+    plugins: [
+        [
+          '@vuepress/pwa',
+          {
+            serviceWorker: true,
+            updatePopup: true
+          }
+        ]
     ],
     themeConfig: {
         sidebar: [
