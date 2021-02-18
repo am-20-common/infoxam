34195e8ceb7f1aa645619093c255f714f541d382
Timur Svoboda <svobodatimur@gmail.com>
2021-02-18 15:04:18 +0200

    Add favicon.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,14 @@
-module.exports = {
+module.exports =  {
     base: '/infoxam/',
     title: 'Infoxam',
     description: 'Infoxam is a static site made to spread the information that applies to AM-201 and AM-202',
+    head: [
+        ['meta', { name: 'theme-color', content: '#ffffff' }],
+        ['link', { rel: 'icon', href: 'favicon.svg' }],
+        ['link', { rel: 'mask-icon', href: 'mask-icon.svg', color: '#000000' }],
+        ['link', { rel: 'apple-touch-icon', href: 'apple-touch-icon.png' }],
+        ['link', { rel: 'manifest', href: 'manifest.json' }],
+    ],
     themeConfig: {
         sidebar: [
             {
